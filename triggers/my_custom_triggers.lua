5d53e7595e657ff92cfae1bc267b082da399407d
Gajo Petrovic <gajo01@gmail.com>
2015-04-13 03:26:33 +0900

    updates

--- triggers/my_custom_triggers.lua
+++ triggers/my_custom_triggers.lua
@@ -3,7 +3,22 @@ return {
 		-- My custom actions go here
 	},
 	functions = {
-		-- My custom functions go here
+        {
+            humanName = "Zombies in area",
+            name = "ZOMBIES_IN_AREA",
+            input = {"area"},
+            output = "unit_array",
+            tags = {"Units"},
+            execute = function()
+                local units = {}
+                for _, unitID in pairs(Spring.GetUnitsInRectangle(unpack(input.area))) do
+                    if UnitDefs[Spring.GetUnitDefID(unitID)].customParams.zombie then
+                        table.insert(units, unitID)
+                    end
+                end
+                return units
+            end
+        },
 	},
 }
 
